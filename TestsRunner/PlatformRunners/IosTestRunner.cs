880c054698605abd22c2e882ae78959066483014
Pavlo Supenko <pavlo.supenko@bini.games>
2022-09-29 10:23:18 +0300

    Change wda local port;

--- TestsRunner/PlatformRunners/IosTestRunner.cs
+++ TestsRunner/PlatformRunners/IosTestRunner.cs
@@ -140,6 +140,7 @@ public class IosTestRunner : ITestsRunner<IosArguments>
         capabilities.AddAdditionalCapability(CustomCapabilityType.TargetDeviceId, deviceId);
         
         capabilities.AddAdditionalCapability("appium:noReset", true);
+        capabilities.AddAdditionalCapability("appium:wdaLocalPort", 8110);
         
         driver = new IOSDriver<IOSElement>(new Uri("http://127.0.0.1:4723/wd/hub"), capabilities, TimeSpan.FromSeconds(600));
         driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
